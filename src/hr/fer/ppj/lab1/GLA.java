d26c18fb54c9c644c81c2e054e4bb8d0db8bbc3c
ivancili <ivan.ilich@hotmail.com>
2017-10-16 12:38:36 +0200

    Added InputProcessor

--- src/hr/fer/ppj/lab1/GLA.java
+++ src/hr/fer/ppj/lab1/GLA.java
@@ -1,24 +1,22 @@
 package hr.fer.ppj.lab1;
 
+import hr.fer.ppj.lab1.helper.InputProcessor;
+
 import java.util.Scanner;
 
 public class GLA {
 
     public static void main(String[] args) {
-
         setupStdIO();
 
         Scanner scanner = new Scanner(System.in);
-        extractData(scanner);
+        InputProcessor inputProcessor = new InputProcessor(scanner);
+
     }
 
     private static void setupStdIO() {
 //        System.setIn();
 //        System.setOut();
-    }
-
-    private static void extractData(Scanner scanner) {
-
     }
 
 }
---
+++ src/hr/fer/ppj/lab1/helper/InputProcessor.java
@@ -0,0 +1,13 @@
+package hr.fer.ppj.lab1.helper;
+
+import java.util.Scanner;
+
+public class InputProcessor {
+
+    private Scanner scanner;
+
+    public InputProcessor(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+}
