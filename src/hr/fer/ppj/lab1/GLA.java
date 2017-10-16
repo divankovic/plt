7aecc2361ed5f8453fecc0c1d40249e0b59dfe49
ivancili <ivan.ilich@hotmail.com>
2017-10-16 12:30:50 +0200

    Refactoring

---
+++ src/hr/fer/ppj/lab1/GLA.java
@@ -0,0 +1,24 @@
+package hr.fer.ppj.lab1;
+
+import java.util.Scanner;
+
+public class GLA {
+
+    public static void main(String[] args) {
+
+        setupStdIO();
+
+        Scanner scanner = new Scanner(System.in);
+        extractData(scanner);
+    }
+
+    private static void setupStdIO() {
+//        System.setIn();
+//        System.setOut();
+    }
+
+    private static void extractData(Scanner scanner) {
+
+    }
+
+}
