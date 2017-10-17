3856afea23a0c9c773ca4ed7f720d2ebb7a0e964
ivancili <ivan.ilich@hotmail.com>
2017-10-17 16:45:25 +0200

    Added implementation for saving data

--- src/hr/fer/ppj/lab1/GLA.java
+++ src/hr/fer/ppj/lab1/GLA.java
@@ -1,11 +1,18 @@
 package hr.fer.ppj.lab1;
 
+import hr.fer.ppj.lab1.helper.EpsilonNFA;
 import hr.fer.ppj.lab1.helper.InputProcessor;
+import hr.fer.ppj.lab1.model.Identifier;
+import hr.fer.ppj.lab1.model.Regex;
+import hr.fer.ppj.lab1.model.Rule;
+import hr.fer.ppj.lab1.model.State;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -49,9 +56,36 @@ public class GLA {
      * Files to save: rules, states, identifiers, automaton for every regex.
      */
     private static void serializeData(InputProcessor inputProcessor) throws IOException {
-        File file = new File(SERIALIZATION_FILE_PATH);
-        FileOutputStream fos = new FileOutputStream(file);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        try {
+
+            File file = new File(SERIALIZATION_FILE_PATH);
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            List<Regex> regexList = inputProcessor.getRegexList();
+            List<State> stateList = inputProcessor.getStateList();
+            List<Identifier> identifierList = inputProcessor.getIdentifierList();
+            List<Rule> ruleList = inputProcessor.getRuleList();
+
+            List<EpsilonNFA> epsilonNFAList = new ArrayList<>();
+            for (Rule rule : ruleList) {
+                epsilonNFAList.add(new EpsilonNFA(rule.getRegex()));
+            }
+
+            oos.writeObject(regexList);
+            oos.writeObject(stateList);
+            oos.writeObject(identifierList);
+            oos.writeObject(ruleList);
+            oos.writeObject(epsilonNFAList);
+
+            fos.close();
+            oos.close();
+
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+
     }
 
 }
