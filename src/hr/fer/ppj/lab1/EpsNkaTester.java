d346b7f563749c7097333bf62823427a1a150990
ivancili <ivan.ilich@hotmail.com>
2017-10-18 20:15:11 +0200

    Cleanup

--- src/hr/fer/ppj/lab1/EpsNkaTester.java
+++ src/hr/fer/ppj/lab1/EpsNkaTester.java
@@ -2,15 +2,17 @@ package hr.fer.ppj.lab1;
 
 import hr.fer.ppj.lab1.helper.EpsilonNFA;
 import hr.fer.ppj.lab1.model.Regex;
-import hr.fer.ppj.lab1.model.TransitionKey;
 
 public class EpsNkaTester {
-    public static void main(String[] args){
-        /*TransitionKey first = new TransitionKey(1,'a');
-        TransitionKey second = new TransitionKey(1,'a');
-        System.out.println(first.equals(second));*/
-        Regex regex = new Regex("a*");
-        EpsilonNFA automaton = new EpsilonNFA(regex);
-        System.out.println(automaton.recognizes("aaaa"));
+
+    public static void main(String[] args) {
+
+        String regexExpression = "(abc)*";
+        String testRegex = "abcabc";
+
+        EpsilonNFA automaton = new EpsilonNFA(new Regex(regexExpression));
+
+        System.out.println(automaton.recognizes(testRegex));
     }
+
 }
