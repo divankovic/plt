66976f9c46e595e1ac878cb5c28d210397907c7a
ivancili <ivan.ilich@hotmail.com>
2017-10-18 20:17:34 +0200

    Code reorganization

--- src/test/EpsNkaTester.java
+++ src/test/EpsNkaTester.java
@@ -1,4 +1,4 @@
-package hr.fer.ppj.lab1;
+package test;
 
 import hr.fer.ppj.lab1.helper.EpsilonNFA;
 import hr.fer.ppj.lab1.model.Regex;
@@ -8,7 +8,7 @@ public class EpsNkaTester {
     public static void main(String[] args) {
 
         String regexExpression = "(abc)*";
-        String testRegex = "abcabc";
+        String testRegex = "abcabcabc";
 
         EpsilonNFA automaton = new EpsilonNFA(new Regex(regexExpression));
 
