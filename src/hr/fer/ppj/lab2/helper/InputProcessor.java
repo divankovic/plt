0c635b14d86c67f6f2fa7d306faad0dea752b1d3
ivancili <ivan.ilich@hotmail.com>
2017-11-04 14:46:53 +0100

    Added class for input processing

---
+++ src/hr/fer/ppj/lab2/helper/InputProcessor.java
@@ -0,0 +1,20 @@
+package hr.fer.ppj.lab2.helper;
+
+import java.util.Scanner;
+
+public class InputProcessor {
+
+    private Scanner scanner;
+
+    public InputProcessor(Scanner scanner) {
+        this.scanner = scanner;
+        startProcessing();
+    }
+
+    /**
+     * Method for extracting regex expressions, states, identifiers and processing rules
+     */
+    private void startProcessing() {
+    }
+
+}
