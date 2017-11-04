06580bf2b5bce48af8d1b25b3a06851d6c689216
ivancili <ivan.ilich@hotmail.com>
2017-11-04 14:47:29 +0100

    Added main method and method for IO redirection

--- src/hr/fer/ppj/lab2/GSA.java
+++ src/hr/fer/ppj/lab2/GSA.java
@@ -1,4 +1,64 @@
 package hr.fer.ppj.lab2;
 
+import hr.fer.ppj.lab2.helper.InputProcessor;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Class for generation of syntax analyser.
+ */
 public class GSA {
+
+    /**
+     * Path to the output file of generator
+     */
+    final static String SERIALIZATION_FILE_PATH = "./src/hr.fer.ppj.lab2.analizator/definition.ser";
+    private final static String TEST_FILE_INPUT_PATH = "./src/hr.fer.ppj.lab2.res/in/";
+    private final static String TEST_FILE_OUTPUT_PATH = "./src/hr.fer.ppj.lab2.res/out/GSA_out.txt";
+
+    public static void main(String[] args) {
+
+        //setupStdIO();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            InputProcessor inputProcessor = new InputProcessor(scanner);
+            serializeData(inputProcessor);
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    /**
+     * Standard I/O redirection
+     */
+    private static void setupStdIO() throws IOException {
+        System.setIn(new FileInputStream(new File(TEST_FILE_INPUT_PATH)));
+        System.setOut(new PrintStream(new File(TEST_FILE_OUTPUT_PATH)));
+    }
+
+    /**
+     * Saving data for LA.
+     * Files to save: rules, states, identifiers, automaton for every regex.
+     */
+    private static void serializeData(InputProcessor inputProcessor) throws IOException {
+
+        try {
+
+            File file = new File(SERIALIZATION_FILE_PATH);
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            fos.close();
+            oos.close();
+
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+
+    }
+
 }
--- src/hr/fer/ppj/lab2/SA.java
+++ src/hr/fer/ppj/lab2/SA.java
@@ -1,4 +1,56 @@
 package hr.fer.ppj.lab2;
 
+import hr.fer.ppj.lab2.helper.InputProcessor;
+
+import java.io.*;
+
+/**
+ * Class for performing syntax analysis.
+ */
 public class SA {
+
+    /**
+     * Path to the output file of generator
+     */
+    final static String SERIALIZATION_FILE_PATH = "./src/hr.fer.ppj.lab2.analizator/definition.ser";
+    private final static String TEST_FILE_INPUT_PATH = "./src/hr.fer.ppj.lab2.res/in/";
+    private final static String TEST_FILE_OUTPUT_PATH = "./src/hr.fer.ppj.lab2.res/out/GSA_out.txt";
+
+    /**
+     * Entry point
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Standard I/O redirection
+     */
+    private static void setupStdIO() throws IOException {
+        System.setIn(new FileInputStream(new File(TEST_FILE_INPUT_PATH)));
+        System.setOut(new PrintStream(new File(TEST_FILE_OUTPUT_PATH)));
+    }
+
+    /**
+     * Saving data for LA.
+     * Files to save: rules, states, identifiers, automaton for every regex.
+     */
+    private static void serializeData(InputProcessor inputProcessor) throws IOException {
+
+        try {
+
+            File file = new File(SERIALIZATION_FILE_PATH);
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            fos.close();
+            oos.close();
+
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+
+    }
+
+
 }
