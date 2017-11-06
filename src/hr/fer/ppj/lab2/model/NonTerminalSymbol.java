2dbbdc30737ed752fbfb885ba43cb664a21762db
ivancili <ivan.ilich@hotmail.com>
2017-11-06 12:46:34 +0100

    Doomsday

--- src/hr/fer/ppj/lab2/model/NonTerminalSymbol.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.ppj.lab2.model;
-
-public class NonTerminalSymbol extends Symbol{
-    public NonTerminalSymbol(String symbol){
-        super(symbol);
-    }
-}
--- src/hr/fer/ppj/lab2/model/SyncTerminalSymbol.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.ppj.lab2.model;
-
-public class SyncTerminalSymbol extends  Symbol{
-    public SyncTerminalSymbol(String symbol){
-        super(symbol);
-    }
-}
