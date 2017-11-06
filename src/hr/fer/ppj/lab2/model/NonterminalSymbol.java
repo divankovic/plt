d1a5e48892e8897221bb5b722514927c2e6a2d0d
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-11-06 10:35:40 +0100

    Added model classes

---
+++ src/hr/fer/ppj/lab2/model/NonterminalSymbol.java
@@ -0,0 +1,17 @@
+package hr.fer.ppj.lab2.model;
+
+public class NonterminalSymbol {
+    private String symbol;
+
+    public NonterminalSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
---
+++ src/hr/fer/ppj/lab2/model/TerminalSymbol.java
@@ -0,0 +1,19 @@
+package hr.fer.ppj.lab2.model;
+
+public class TerminalSymbol {
+
+    private String symbol;
+
+    public TerminalSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+}
