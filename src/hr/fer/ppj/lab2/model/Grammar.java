1f7512779ef36474a3e1c0644db70b12d0379fae
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-11-06 12:09:24 +0100

    Added model classes

--- src/hr/fer/ppj/lab2/model/Grammar.java
+++ src/hr/fer/ppj/lab2/model/Grammar.java
@@ -1,21 +1,31 @@
 package hr.fer.ppj.lab2.model;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import com.sun.scenario.effect.impl.state.LinearConvolveKernel;
+
+import java.util.*;
 
 public class Grammar {
 
     private HashMap<NonTerminalSymbol, List<GrammarProduction>> productionMap;
     private HashMap<NonTerminalSymbol, List<GrammarProduction>> dottedProductionMap;
 
-    public Grammar(HashMap<NonTerminalSymbol, List<GrammarProduction> productionMap) {
+    public Grammar(HashMap<NonTerminalSymbol, List<GrammarProduction>> productionMap) {
         this.productionMap = productionMap;
         createDottedProductionMap();
     }
 
     private void createDottedProductionMap() {
+        for(Map.Entry<NonTerminalSymbol,List<GrammarProduction>> entry : productionMap.entrySet()){
+            List<GrammarProduction> productions = entry.getValue();
+            List<GrammarProduction> dottedProductions = new LinkedList<>();
+            productions.forEach(production->{
+                int position = 0;
+                int lenght = production.getRightSide().size();
+                while(position<=lenght){
 
+                }
+            });
+        }
     }
 
 
---
+++ src/hr/fer/ppj/lab2/model/SyncTerminalSymbol.java
@@ -0,0 +1,7 @@
+package hr.fer.ppj.lab2.model;
+
+public class SyncTerminalSymbol extends  Symbol{
+    public SyncTerminalSymbol(String symbol){
+        super(symbol);
+    }
+}
