02dae1076f8dc54b4eb98aad3a63ad3f8717775d
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-11-06 12:36:06 +0100

    Added model classes

--- src/hr/fer/ppj/lab2/model/Grammar.java
+++ src/hr/fer/ppj/lab2/model/Grammar.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Grammar {
 
+    private static String dotSymbol = "*";
     private HashMap<NonTerminalSymbol, List<GrammarProduction>> productionMap;
     private HashMap<NonTerminalSymbol, List<GrammarProduction>> dottedProductionMap;
 
@@ -16,14 +17,28 @@ public class Grammar {
 
     private void createDottedProductionMap() {
         for(Map.Entry<NonTerminalSymbol,List<GrammarProduction>> entry : productionMap.entrySet()){
+            NonTerminalSymbol keySymbol = entry.getKey();
             List<GrammarProduction> productions = entry.getValue();
             List<GrammarProduction> dottedProductions = new LinkedList<>();
+
             productions.forEach(production->{
                 int position = 0;
-                int lenght = production.getRightSide().size();
-                while(position<=lenght){
-
+                int length = production.getRightSide().size();
+                while(position<length){
+                    List<String> dottedRightSide = new LinkedList<>();
+                    dottedRightSide.addAll(production.getRightSide());
+                    dottedRightSide.add(position,dotSymbol);
+                    GrammarProduction dottedProduction = new GrammarProduction(production.getLeftSide(),dottedRightSide);
+                    dottedProductions.add(dottedProduction);
+                    position +=2;
                 }
+                List<String> dottedRightSide = new LinkedList<>();
+                dottedRightSide.addAll(production.getRightSide());
+                dottedRightSide.add(dotSymbol);
+                dottedProductions.add(new GrammarProduction(production.getLeftSide(),dottedRightSide));
+
+                dottedProductionMap.put(keySymbol,dottedProductions);
+
             });
         }
     }
--- src/hr/fer/ppj/lab2/model/GrammarProduction.java
+++ src/hr/fer/ppj/lab2/model/GrammarProduction.java
@@ -3,27 +3,27 @@ package hr.fer.ppj.lab2.model;
 import java.util.List;
 
 public class GrammarProduction {
-    private String leftSide;
-    private List<String> rightSide;
+    private NonTerminalSymbol leftSide;
+    private List<Symbol> rightSide;
 
-    public GrammarProduction(String leftSide, List<String> rightSide) {
+    public GrammarProduction(NonTerminalSymbol leftSide, List<Symbol> rightSide) {
         this.leftSide = leftSide;
         this.rightSide = rightSide;
     }
 
-    public String getLeftSide() {
+    public NonTerminalSymbol getLeftSide() {
         return leftSide;
     }
 
-    public void setLeftSide(String leftSide) {
+    public void setLeftSide(NonTerminalSymbol leftSide) {
         this.leftSide = leftSide;
     }
 
-    public List<String> getRightSide() {
+    public List<Symbol> getRightSide() {
         return rightSide;
     }
 
-    public void setRightSide(List<String> rightSide) {
+    public void setRightSide(List<Symbol> rightSide) {
         this.rightSide = rightSide;
     }
 }
