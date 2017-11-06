d7624dc0ae00716dda048dfe2658b03b4a2742c8
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-11-06 11:52:33 +0100

    Added model classes

--- src/hr/fer/ppj/lab2/model/Grammar.java
+++ src/hr/fer/ppj/lab2/model/Grammar.java
@@ -6,33 +6,16 @@ import java.util.List;
 
 public class Grammar {
 
-    private HashMap<String, List<GrammarProduction>> productions;
-    private HashMap<String, String> productionMap;
+    private HashMap<NonTerminalSymbol, List<GrammarProduction>> productionMap;
+    private HashMap<NonTerminalSymbol, List<GrammarProduction>> dottedProductionMap;
 
-    public Grammar(HashMap<String, String> productionMap) {
+    public Grammar(HashMap<NonTerminalSymbol, List<GrammarProduction> productionMap) {
         this.productionMap = productionMap;
-
+        createDottedProductionMap();
     }
 
-    public Grammar(List<String> nonTerminalSymbols, List<GrammarProduction> grammarProductions) {
-        productions = new HashMap<>();
-        fillMap(nonTerminalSymbols, grammarProductions);
-    }
+    private void createDottedProductionMap() {
 
-    private void fillMap(List<String> nonTerminalSymbols, List<GrammarProduction> grammarProductions) {
-        nonTerminalSymbols.forEach(nonTerminalSymbol -> {
-            grammarProductions.forEach(grammarProduction -> {
-                if (grammarProduction.getLeftSide().equals(nonTerminalSymbol)) {
-                    List<GrammarProduction> productionList = productions.get(nonTerminalSymbol);
-                    if (productionList == null) {
-                        productionList = new LinkedList<GrammarProduction>();
-                        productionList.add(grammarProduction);
-                    } else if (!productionList.contains(grammarProduction)) {
-                        productionList.add(grammarProduction);
-                    }
-                }
-            });
-        });
     }
 
 
