e8f58ef2c0678d030ee74f7378e8d2c0cc0fda9a
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-11-06 17:55:52 +0100

    Added class DFA

---
+++ src/hr/fer/ppj/lab2/model/DFA.java
@@ -0,0 +1,14 @@
+package hr.fer.ppj.lab2.model;
+
+import hr.fer.ppj.lab1.helper.EpsilonNFA;
+
+public class DFA {
+
+    public DFA(EpsilonNFA epsilonNFA){
+        convert();
+    }
+
+    private void convert() {
+
+    }
+}
