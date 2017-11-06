083cef644a91356c9be7f0d49e14fc8b804cb48a
ivancili <ivan.ilich@hotmail.com>
2017-11-06 11:28:20 +0100

    Work on input processing

--- src/hr/fer/ppj/lab2/helper/InputProcessor.java
+++ src/hr/fer/ppj/lab2/helper/InputProcessor.java
@@ -1,13 +1,27 @@
 package hr.fer.ppj.lab2.helper;
 
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * Class for input processing
+ */
 public class InputProcessor {
 
     private Scanner scanner;
+    private List<String> nonterminalSymbols;
+    private List<String> terminalSymbols;
+    private List<String> syncSymbols;
+    private HashMap<String, String> productionsMap;
 
+    /**
+     *
+     */
     public InputProcessor(Scanner scanner) {
         this.scanner = scanner;
+        this.nonterminalSymbols = new LinkedList<>();
+        this.terminalSymbols = new LinkedList<>();
+        this.syncSymbols = new LinkedList<>();
+        this.productionsMap = new HashMap<>();
         startProcessing();
     }
 
@@ -21,19 +35,31 @@ public class InputProcessor {
 
         line = scanner.nextLine();
         parts = line.replace("%V", "").trim().split("\\s+");
-        // Collections.addAll(nezavrsniZnakovi, parts);
+        Collections.addAll(nonterminalSymbols, parts);
 
         line = scanner.nextLine();
         parts = line.replace("%T", "").trim().split("\\s+");
-        // Collections.addAll(zavrsniZnakovi, parts);
+        Collections.addAll(terminalSymbols, parts);
 
         line = scanner.nextLine();
         parts = line.replace("%Syn", "").trim().split("\\s+");
-        // Collections.addAll(syncZnakovi, parts);
+        Collections.addAll(syncSymbols, parts);
 
-        // produkcije
         while (scanner.hasNextLine()) {
+
+            String key = null;
+            String value = null;
+
             line = scanner.nextLine();
+
+            if (line.startsWith("\\s+")) {
+                key = line;
+                continue;
+            } else {
+                value = line;
+            }
+
+            productionsMap.put(key, value);
         }
 
     }
