825fcc577bbbc42e3348e56420407de9aa409f4e
ivancili <ivan.ilich@hotmail.com>
2017-12-28 09:59:42 +0100

    Input/output setup

--- src/hr/fer/ppj/lab3/SemantickiAnalizator.java
+++ src/hr/fer/ppj/lab3/SemantickiAnalizator.java
@@ -3,26 +3,37 @@ package hr.fer.ppj.lab3;
 import hr.fer.ppj.lab3.model.NonterminalSymbol;
 import hr.fer.ppj.lab3.model.Production;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 public class SemantickiAnalizator {
+
+    private static final String TEST_FILE_INPUT_PATH = "./src/hr/fer/ppj/lab3/res/in/in.txt";
+    private static final String TEST_FILE_OUTPUT_PATH = "./src/hr/fer/ppj/lab3/res/out/out.txt";
+
     private static List<Production> productions;
     private static List<String> generatingTree;
     private static NonterminalSymbol startingSymbol;
 
-
-    public void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        setupStdIO();
         readFromInput();
         fillProductions();
         //build tree ( startingSymbol )
     }
 
-    private void readFromInput() {
+    private static void setupStdIO() throws IOException {
+        System.setIn(new FileInputStream(new File(TEST_FILE_INPUT_PATH)));
+        System.setOut(new PrintStream(new File(TEST_FILE_OUTPUT_PATH)));
     }
 
-    private void fillProductions() {
-
+    private static void readFromInput() {
     }
 
+    private static void fillProductions() {
+    }
 
 }
