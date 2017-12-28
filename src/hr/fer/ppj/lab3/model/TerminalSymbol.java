5f11509f20f108d023e0f4b54902c0b3d88a62fe
ivancili <ivan.ilich@hotmail.com>
2017-12-28 11:50:19 +0100

    Further implementation

--- src/hr/fer/ppj/lab3/model/TerminalSymbol.java
+++ src/hr/fer/ppj/lab3/model/TerminalSymbol.java
@@ -4,6 +4,10 @@ public class TerminalSymbol extends Symbol {
 
     String value;
 
+    public TerminalSymbol(String name) {
+        super(name);
+    }
+
     public TerminalSymbol(String name, String value) {
         super(name);
         this.value = value;
