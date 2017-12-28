5c14cfb593209dcf2b41ff60a6b88694723bfbd3
Dorian Ivankovic <dorian.ivankovic@fer.hr>
2017-12-28 11:38:44 +0100

    Further implementation

--- src/hr/fer/ppj/lab3/model/Symbol.java
+++ src/hr/fer/ppj/lab3/model/Symbol.java
@@ -1,11 +1,25 @@
 package hr.fer.ppj.lab3.model;
 
+import java.util.List;
+
 public abstract class Symbol {
 
-    String name;
+    private String name;
+    private List<Symbol> childrenSymbols;
 
     public Symbol(String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public List<Symbol> getChildrenSymbols() {
+        return childrenSymbols;
+    }
+
+    public void setChildrenSymbols(List<Symbol> childrenSymbols) {
+        this.childrenSymbols = childrenSymbols;
+    }
 }
