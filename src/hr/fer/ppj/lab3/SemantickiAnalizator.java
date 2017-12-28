0eb15111371851f695b63b52f5233a3134476bc6
ivancili <ivan.ilich@hotmail.com>
2017-12-28 12:55:55 +0100

    Further implementation

--- src/hr/fer/ppj/lab3/SemantickiAnalizator.java
+++ src/hr/fer/ppj/lab3/SemantickiAnalizator.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,6 +20,9 @@ public class SemantickiAnalizator {
     private static List<String> input;
     private static Element startingElement;
 
+    /**
+     *
+     */
     public static void main(String[] args) throws IOException {
         setupStdIO();
         readFromInput();
@@ -26,11 +30,17 @@ public class SemantickiAnalizator {
         //build tree ( startingSymbol )
     }
 
+    /**
+     *
+     */
     private static void setupStdIO() throws IOException {
         System.setIn(new FileInputStream(new File(TEST_FILE_INPUT_PATH)));
         System.setOut(new PrintStream(new File(TEST_FILE_OUTPUT_PATH)));
     }
 
+    /**
+     *
+     */
     private static void readFromInput() {
 
         try (Scanner scanner = new Scanner(System.in)) {
@@ -47,13 +57,16 @@ public class SemantickiAnalizator {
         buildGeneratingTree();
     }
 
+    /**
+     *
+     */
     private static void buildGeneratingTree() {
         int lineIdx = 0;
-        startingElement = new Element(new NonterminalSymbol(input.get(lineIdx)),lineIdx);
+        startingElement = new Element(new NonterminalSymbol(input.get(lineIdx)), lineIdx);
         List<Element> newElements = new LinkedList<>();
         newElements.add(startingElement);
 
-        while(!newElements.isEmpty()) {
+        while (!newElements.isEmpty()) {
             List<Element> newElementsTemp = new LinkedList<>();
             for (Element element : newElements) {
                 List<Element> childrenElements = getChildrenElements(element);
@@ -68,22 +81,25 @@ public class SemantickiAnalizator {
 
     }
 
+    /**
+     *
+     */
     private static List<Element> getChildrenElements(Element element) {
         int lineIdx = element.getLineIdx();
         int indentation = getIndentation(input.get(lineIdx));
         List<Element> childrenElements = new LinkedList<>();
 
-        for(int i=lineIdx+1;i<input.size();i++){
+        for (int i = lineIdx + 1; i < input.size(); i++) {
             String line = input.get(i);
-            if(getIndentation(line)<=indentation){
+            if (getIndentation(line) <= indentation) {
                 break;
             }
-            if(getIndentation(line)==indentation+1){
+            if (getIndentation(line) == indentation + 1) {
                 String content = line.trim();
-                if(content.startsWith("<")) {
-                    childrenElements.add(new Element(new NonterminalSymbol(content),i));
-                }else{
-                    childrenElements.add(new Element(new TerminalSymbol(content),i));
+                if (content.startsWith("<")) {
+                    childrenElements.add(new Element(new NonterminalSymbol(content), i));
+                } else {
+                    childrenElements.add(new Element(new TerminalSymbol(content), i));
                 }
             }
         }
@@ -91,14 +107,20 @@ public class SemantickiAnalizator {
 
     }
 
-    private static int getIndentation(String line){
+    /**
+     *
+     */
+    private static int getIndentation(String line) {
         int i = 0;
-        while(Character.isWhitespace(line.charAt(i))){
+        while (Character.isWhitespace(line.charAt(i))) {
             ++i;
         }
         return i;
     }
 
+    /**
+     *
+     */
     private static void printGeneratingTree(Element element, int level) {
         System.out.println(getIndentation(level) + element.getSymbol().getName());
         List<Element> childrenElements = element.getChildrenElements();
@@ -109,6 +131,9 @@ public class SemantickiAnalizator {
         }
     }
 
+    /**
+     *
+     */
     private static String getIndentation(int level) {
         String indentation = "";
         int i = 0;
@@ -119,7 +144,155 @@ public class SemantickiAnalizator {
         return indentation;
     }
 
+    /**
+     *
+     */
     private static void fillProductions() {
+        productions = new ArrayList<>();
+
+        addNewProduction("<primarni_izraz>", "IDN");
+        addNewProduction("<primarni_izraz>", "BROJ");
+        addNewProduction("<primarni_izraz>", "ZNAK");
+        addNewProduction("<primarni_izraz>", "NIZ_ZNAKOVA");
+        addNewProduction("<primarni_izraz>", "L_ZAGRADA <izraz> D_ZAGRADA");
+
+        addNewProduction("<postfiks_izraz>", "<primarni_izraz>");
+        addNewProduction("<postfiks_izraz>", "<postfiks_izraz> L_UGL_ZAGRADA <izraz> D_UGL_ZAGRADA");
+        addNewProduction("<postfiks_izraz>", "<postfiks_izraz> L_ZAGRADA D_ZAGRADA");
+        addNewProduction("<postfiks_izraz>", "<postfiks_izraz> L_ZAGRADA <lista_argumenata> D_ZAGRADA");
+        addNewProduction("<postfiks_izraz>", "<postfiks_izraz> OP_INC");
+        addNewProduction("<postfiks_izraz>", "<postfiks_izraz> OP_DEC");
+
+        addNewProduction("<lista_argumenata>", "<izraz_pridruzivanja>");
+        addNewProduction("<lista_argumenata>", "<lista_argumenata> ZAREZ <izraz_pridruzivanja>");
+
+        addNewProduction("<unarni_izraz>", "<postfiks_izraz>");
+        addNewProduction("<unarni_izraz>", "OP_INC <unarni_izraz>");
+        addNewProduction("<unarni_izraz>", "OP_DEC <unarni_izraz>");
+        addNewProduction("<unarni_izraz>", "<unarni_operator> <cast_izraz>");
+
+        addNewProduction("<cast_izraz>", "<unarni_izraz>");
+        addNewProduction("<cast_izraz>", "L_ZAGRADA <ime_tipa> D_ZAGRADA <cast_izraz>");
+
+        addNewProduction("<ime_tipa>", "<specifikator_tipa>");
+        addNewProduction("<ime_tipa>", "KR_CONST <specifikator_tipa>");
+
+        addNewProduction("<specifikator_tipa>", "KR_VOID");
+        addNewProduction("<specifikator_tipa>", "KR_CHAR");
+        addNewProduction("<specifikator_tipa>", "KR_INT");
+
+        addNewProduction("<multiplikativni_izraz>", "<cast_izraz>");
+        addNewProduction("<multiplikativni_izraz>", "<multiplikativni_izraz> OP_PUTA <cast_izraz>");
+        addNewProduction("<multiplikativni_izraz>", "<multiplikativni_izraz> OP_DIJELI <cast_izraz>");
+        addNewProduction("<multiplikativni_izraz>", "<multiplikativni_izraz> OP_MOD <cast_izraz>");
+
+        addNewProduction("<aditivni_izraz>", "<multiplikativni_izraz>");
+        addNewProduction("<aditivni_izraz>", "<aditivni_izraz> (PLUS | MINUS) <multiplikativni_izraz>");
+
+        addNewProduction("<odnosni_izraz>", "<aditivni_izraz>");
+        addNewProduction("<odnosni_izraz>", "<odnosni_izraz> OP_LT <aditivni_izraz>");
+        addNewProduction("<odnosni_izraz>", "<odnosni_izraz> OP_GT <aditivni_izraz>");
+        addNewProduction("<odnosni_izraz>", "<odnosni_izraz> OP_LTE <aditivni_izraz>");
+        addNewProduction("<odnosni_izraz>", "<odnosni_izraz> OP_GTE<aditivni_izraz>");
+
+        addNewProduction("<jednakosni izraz>", "<odnosni_izraz>");
+        addNewProduction("<jednakosni izraz>", "<jednakosni_izraz> OP_EQ <odnosni_izraz>");
+        addNewProduction("<jednakosni izraz>", "<jednakosni_izraz> OP_NEQ <odnosni_izraz>");
+
+        addNewProduction("<bin_i_izraz>", "<jednakosni_izraz>");
+        addNewProduction("<bin_i_izraz>", "<bin_i_izraz> OP_BIN_I <jednakosni_izraz>");
+
+        addNewProduction("<bin xili izraz>", "<bin_i_izraz>");
+        addNewProduction("<bin xili izraz>", "<bin_xili_izraz> OP_BIN_XILI <bin_i_izraz>");
+
+        addNewProduction("<bin ili izraz>", "<bin_xili_izraz>");
+        addNewProduction("<bin ili izraz>", "<bin_ili_izraz> OP_BIN_ILI <bin_xili_izraz>");
+
+        addNewProduction("<log i izraz>", "<bin_ili_izraz>");
+        addNewProduction("<log i izraz>", "<log_i_izraz> OP_I <bin_ili_izraz>");
+
+        addNewProduction("<log ili izraz>", "<log_i_izraz>");
+        addNewProduction("<log ili izraz>", "<log_ili_izraz> OP_ILI <log_i_izraz>");
+
+        addNewProduction("<izraz_pridruzivanja>", "<log_ili_izraz>");
+        addNewProduction("<izraz_pridruzivanja>", "<postfiks_izraz> OP_PRIDRUZI <izraz_pridruzivanja>");
+
+        addNewProduction("<izraz>", "<izraz_pridruzivanja>");
+        addNewProduction("<izraz>", "<izraz> ZAREZ <izraz_pridruzivanja>");
+
+        addNewProduction("<slozena naredba>", "L_VIT_ZAGRADA <lista_naredbi> D_VIT_ZAGRADA");
+        addNewProduction("<slozena naredba>", "L_VIT_ZAGRADA <lista_deklaracija> <lista_naredbi> D_VIT_ZAGRADA");
+
+        addNewProduction("<lista_naredbi>", "<naredba>");
+        addNewProduction("<lista_naredbi>", "<lista_naredbi> <naredba>");
+
+        addNewProduction("<izraz naredba>", "TOCKAZAREZ");
+        addNewProduction("<izraz naredba>", "<izraz> TOCKAZAREZ");
+
+        addNewProduction("<naredba_grananja>", "KR_IF L_ZAGRADA <izraz> D_ZAGRADA <naredba>");
+        addNewProduction("<naredba_grananja>", "KR_IF L_ZAGRADA <izraz> D_ZAGRADA <naredba> KR_ELSE <naredba>");
+
+        addNewProduction("<naredba petlje>", "KR_WHILE L_ZAGRADA <izraz> D_ZAGRADA <naredba>");
+        addNewProduction("<naredba petlje>", "KR_FOR L_ZAGRADA <izraz_naredba> <izraz_naredba> D_ZAGRADA <naredba>");
+        addNewProduction("<naredba petlje>", "KR_FOR L_ZAGRADA <izraz_naredba> <izraz_naredba> <izraz> D_ZAGRADA <naredba>");
+
+        addNewProduction("<naredba skoka>", "KR_CONTINUE TOCKAZAREZ");
+        addNewProduction("<naredba skoka>", "KR_BREAK TOCKAZAREZ");
+        addNewProduction("<naredba skoka>", "KR_RETURN TOCKAZAREZ");
+        addNewProduction("<naredba skoka>", "KR_RETURN <izraz> TOCKAZAREZ");
+
+        addNewProduction("<prijevodna jedinica>", "<vanjska_deklaracija>");
+        addNewProduction("<prijevodna jedinica>", "<prijevodna_jedinica> <vanjska_deklaracija>");
+
+        addNewProduction("<definicija funkcije>", "<ime_tipa> IDN L_ZAGRADA KR_VOID D_ZAGRADA <slozena_naredba>");
+        addNewProduction("<definicija funkcije>", "<ime_tipa> IDN L_ZAGRADA <lista_parametara> D_ZAGRADA <slozena_naredba>");
+
+        addNewProduction("<lista parametara>", "<deklaracija_parametra>");
+        addNewProduction("<lista parametara>", "<lista_parametara> ZAREZ <deklaracija_parametra>");
+
+        addNewProduction("<deklaracija parametra>", "<ime_tipa> IDN");
+        addNewProduction("<deklaracija parametra>", "<ime_tipa> IDN L_UGL_ZAGRADA D_UGL_ZAGRADA");
+
+        addNewProduction("<lista deklaracija>", "<deklaracija>");
+        addNewProduction("<lista deklaracija>", "<lista_deklaracija> <deklaracija>");
+
+        addNewProduction("<deklaracija>", "<ime_tipa> <lista_init_deklaratora> TOCKAZAREZ");
+
+        addNewProduction("<lista init deklaratora>", "<init_deklarator>");
+        addNewProduction("<lista init deklaratora>", "<lista_init_deklaratora> ZAREZ <init_deklarator>");
+
+        addNewProduction("<init deklarator>", "<izravni_deklarator>");
+        addNewProduction("<init deklarator>", "<izravni_deklarator> OP_PRIDRUZI <inicijalizator>");
+
+        addNewProduction("<izravni deklarator>", "IDN");
+        addNewProduction("<izravni deklarator>", "IDN L_UGL_ZAGRADA BROJ D_UGL_ZAGRADA");
+        addNewProduction("<izravni deklarator>", "IDN L_ZAGRADA KR_VOID D_ZAGRADA");
+        addNewProduction("<izravni deklarator>", "IDN L_ZAGRADA <lista_parametara> D_ZAGRADA");
+        addNewProduction("<izravni deklarator>", "IDN L_UGL_ZAGRADA BROJ D_UGL_ZAGRADA");
+
+        addNewProduction("<inicijalizator>", "<izraz_pridruzivanja>");
+        addNewProduction("<inicijalizator>", "L_VIT_ZAGRADA <lista_izraza_pridruzivanja> D_VIT_ZAGRADA");
+
+        addNewProduction("<lista izraza pridruzivanja>", "<izraz_pridruzivanja>");
+        addNewProduction("<lista izraza pridruzivanja>", "<lista_izraza_pridruzivanja> ZAREZ <izraz_pridruzivanja>");
+    }
+
+    private static void addNewProduction(String leftSide, String rightSides) {
+
+        NonterminalSymbol leftSideOfProd = new NonterminalSymbol(leftSide);
+        List<Symbol> rightSideOfProd = new ArrayList<>();
+
+        for (String symbol : rightSides.split("\\s+")) {
+
+            if (symbol.startsWith("<")) {
+                rightSideOfProd.add(new NonterminalSymbol(symbol));
+            } else {
+                rightSideOfProd.add(new TerminalSymbol(symbol));
+            }
+
+        }
+
+        productions.add(new Production(leftSideOfProd, rightSideOfProd));
     }
 
 }
